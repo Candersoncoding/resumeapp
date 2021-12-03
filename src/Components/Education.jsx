6d7f6473d1f444f9e0fa15809ba146b7b22fc9d1
Candersoncoding <soccercda88@aol.com>
2021-12-03 00:33:48 -0600

    updated education component and added buttons

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -5,6 +5,7 @@ const Education = () => {
 
     const [showCertificate, setShowCertificate] = useState(false);
     const [certificate, setCertificate] = useState(certificateOfAchievement)
+    
     const onClickCerticateButton = () => {
         setShowCertificate(true)
     }
@@ -16,12 +17,12 @@ const Education = () => {
                     <h5 className="display-6 mb-3">Coding Dojo</h5>
                     <hr className="my-2"></hr>
                     <div className="educationFooter">
-                        <div className="splitEducationFooter">
+                        <div className="columnizeEducationFooter">
                             <h4>Program: Full-Stack Developer</h4>
                             <h4>Attended: March/2021 - July/2021</h4>
                             <h4>Field of Study: Computer Science</h4>
                         </div>
-                        <div className="splitEducationFooter">
+                        <div className="columnizeEducationFooter">
                             <h4>Core Studies:</h4>
                             <ul>
                                 <li>Web Fundementals (HTML5, CSS3, jQuery)</li>
@@ -31,10 +32,32 @@ const Education = () => {
                             </ul>
                         </div>                      
                     </div>
+                    
                     <hr></hr>
                     
                 {showCertificate? <img src={certificate} className="certificateImage" /> : <button className="btn btn-outline-light" onClick={onClickCerticateButton}>Certificate of Achievement</button>}
+                <hr></hr>
+                        <h5 className="display-6 mb-3">Codecademy</h5>
+                        <hr className="my-2"></hr>
+                        <div className="educationFooter">
+                            <div className="columnizeEducationFooter">
+                                    <ul>
+                                        <li><h4>jQuery</h4></li>
+                                        <li><h4>Asynchronous JavaScript</h4></li>
+                                        <li><h4>Statistics with NumPy</h4></li>
+                                    </ul>
+                            </div>                      
+                        </div>
+                        <hr></hr>
+                    <nav className="codecademyButtonContainer">    
+                    {showCertificate? <img src={certificate} className="certificateImage" /> : <button className="btn btn-outline-light" onClick={onClickCerticateButton}>Certificate of jQuery</button>}
+                    {showCertificate? <img src={certificate} className="certificateImage" /> : <button className="btn btn-outline-light" onClick={onClickCerticateButton}>Certificate of Asynchronous JS</button>}
+                    {showCertificate? <img src={certificate} className="certificateImage" /> : <button className="btn btn-outline-light" onClick={onClickCerticateButton}>Certificate of Stats with NumPy</button>}
+                    </nav>
                 </div>
+                </div>
+                <div className="jumbotronContainer">
+                
             </div>
         </div>
     )
